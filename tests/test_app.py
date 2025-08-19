836c6f079a8b13171f48cd9439e677b23897dcce
dany_cl <jdanehirmanse@gmail.com>
2025-08-19 10:07:27 +0300

    Test déploiement automatique

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,12 @@
 import io
 import os
 import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from my_app import app
+
 
 @pytest.fixture
 def client(tmp_path):
